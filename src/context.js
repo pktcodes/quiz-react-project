6fcda6072604306ab5f75fbbbb3e2f8a067600d4
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-07-11 18:54:22 -0500

    Fetch Questions

--- src/context.js
+++ src/context.js
@@ -25,6 +25,33 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const fetchQuestions = async (url) => {
+    setLoading(false);
+    setWaiting(false);
+    const response = await axios(url).catch((error) => {
+      console.log(error);
+    });
+    if (response) {
+      const data = response.data.results;
+
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
