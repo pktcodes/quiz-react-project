2c5af03b685a45e5430a706d04ab37112b8f38b2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-07-14 00:23:10 -0500

    Randomize Correct Answer

--- src/App.js
+++ src/App.js
@@ -24,7 +24,15 @@ function App() {
   }
 
   const { correct_answer, incorrect_answers, question } = questions[index];
-  const answers = [...incorrect_answers, correct_answer];
+  // const answers = [...incorrect_answers, correct_answer];
+  let answers = [...incorrect_answers];
+  const tempIndex = Math.floor(Math.random() * 4);
+  if (tempIndex === 3) {
+    answers.push(correct_answer);
+  } else {
+    answers.push(answers[tempIndex]);
+    answers[tempIndex] = correct_answer;
+  }
 
   return (
     <main>
